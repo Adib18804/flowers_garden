4fddbbeb1146418342fc5e19184eb7c1e25fa141
Mohammad Adib Abtahi <maabtahi818@gmail.com>
2025-05-30 22:18:42 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-onload = () =>{
-    document.body.classList.remove("container");
+window.onload = () => {
+  document.body.classList.remove("container");
+  document.body.style.display = "flex";
+  document.body.style.alignItems = "flex-end";
+  document.body.style.justifyContent = "center";
+  document.body.style.minHeight = "100vh";
+  document.body.style.backgroundColor = "#000";
+  document.body.style.overflow = "hidden";
+  document.body.style.perspective = "1000px";
 };
